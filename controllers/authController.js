9186dc3b12f703419451fa1dae4f35c0645c67dd
Andy-git985 <andy.yu617@gmail.com>
2024-01-30 20:15:50 -0500

    logout route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+import logger from '../utils/logger/index.js';
 import { authenticateUser } from '../services/authService.js';
 
 /**
@@ -20,4 +21,31 @@ export const login = async (req, res) => {
  */
 export const getAccount = async (req, res) => {
   res.json(req.session);
+};
+
+/**
+ * @description login user
+ * @route /login
+ * @method POST
+ * @returns {Response | Error} 204 for successful response
+ */
+export const logout = async (req, res) => {
+  console.log('received');
+  const { user } = req.session;
+  req.session.destroy((err) => {
+    if (err) {
+      logger.error('Error performing logout:');
+      logger.error(err);
+    } else if (user) {
+      logger.info(`Logged out user ${user.email}.`);
+    } else {
+      logger.info('Logout called by a user without a session.');
+    }
+  });
+  res.clearCookie('cutabove', {
+    httpOnly: true,
+    sameSite: 'lax',
+    secure: false, // if true: only transmit cookie over https, in prod, always activate this
+  });
+  res.status(204).end();
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import authenticateUser from '../middlewares/authenticateUser.js';
-import { login, getAccount } from '../controllers/authController.js';
+import { login, getAccount, logout } from '../controllers/authController.js';
 
 const router = Router();
 
 // TODO: add validation
 router.route('/login').post(login);
 router.route('/account').get(authenticateUser, getAccount);
+router.route('/logout').get(logout);
 
 export default router;
