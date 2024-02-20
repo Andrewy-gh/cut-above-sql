99de9dde6961693d691f5c617bc79114aceb2fe8
Andy-git985 <andy.yu617@gmail.com>
2024-02-19 19:28:14 -0500

    added import

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,6 +14,7 @@ import validateToken from '../middlewares/validateToken.js';
 import {
   loginSchema,
   signupSchema,
+  emailSchema,
   passwordSchema,
   tokenUrlSchema,
 } from '../schemas/authSchema.js';
